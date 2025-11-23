3878532c3e0633c8b4aafc87467b5be36f6297b7
Raul Abakarov <lionn@Rauls-MacBook-Air.local>
2025-11-23 12:05:57 +0400

    Revert "Redesign scroll to top button with modern minimalist style"
    This reverts commit c006896025aa76c7026458a56bcaf546b4c96ed8.

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -27,7 +27,7 @@ export const ScrollToTop: React.FC = () => {
       onClick={scrollToTop}
       aria-label="Scroll to top"
     >
-      <ChevronUp size={20} strokeWidth={2.5} />
+      <ChevronUp size={24} />
     </button>
   );
 };
