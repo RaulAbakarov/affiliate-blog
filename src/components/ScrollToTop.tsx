79ec0a58b92e6a0169f7eae94f4066250e533521
Raul Abakarov <lionn@Rauls-MacBook-Air.local>
2025-11-23 12:13:21 +0400

    Remove unused ChevronUp import

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { ChevronUp } from 'lucide-react';
 import styles from './ScrollToTop.module.css';
 
 export const ScrollToTop: React.FC = () => {
